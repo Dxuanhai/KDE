efb9da686248f8973e8f1a05c2d2ff0275ad9495
xuanhaibaka <dauhai2005@gmail.com>
2024-03-18 15:06:10 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button className="text-blue-300">KDE PROJECT | TAILWINDCSS</Button>
+    </>
+  );
+}
+
+export default App;
